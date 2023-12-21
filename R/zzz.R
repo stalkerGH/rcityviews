8f77243af233ac811e851c8d63f4abae8fafabe9
stalkerGH <stalker@poczta.onet.pl>
2023-12-21 14:22:46 +0100

    Update zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -22,6 +22,7 @@
 #   sysfonts::font_add_google("Fredericka the Great")
 #   sysfonts::font_add_google("Dancing Script")
 #   sysfonts::font_add_google("Walter Turncoat")
+#   sysfonts::font_add_google("Permanent Marker")
 #   sysfonts::font_add("American Uncial Regular", system.file("fonts", "uncial.otf", package = "rcityviews"))
 #   sysfonts::font_add("Rage", system.file("fonts", "rage.ttf", package = "rcityviews"))
   showtext::showtext_auto(enable = TRUE)
